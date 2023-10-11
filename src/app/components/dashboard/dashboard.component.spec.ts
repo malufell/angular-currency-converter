d1d386b91291c1a755a3c8d0ba9fb8c18e41ec57
Malu <malufell@hotmail.com>
2023-10-11 12:45:52 -0300

    test: add unit tests to dashboard-component

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard.component';
 import { CurrencyCardComponent } from '../currency-card/currency-card.component';
+import { CurrencyService } from '../../services/currency.service';
 
 import { registerLocaleData } from '@angular/common';
 import pt from '@angular/common/locales/pt';
@@ -11,12 +12,16 @@ registerLocaleData(pt);
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let currencyService: CurrencyService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
       declarations: [DashboardComponent, CurrencyCardComponent],
+      providers: [CurrencyService],
     });
+
+    currencyService = TestBed.inject(CurrencyService);
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -24,5 +29,29 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display loading state', () => {
+    component.isLoading = true;
+    fixture.detectChanges();
+    const loaderElement = fixture.nativeElement.querySelector('.card__loader');
+    expect(loaderElement).toBeTruthy();
+  });
+
+  it('should display currency data after loading', () => {
+    component.isLoading = false;
+    component.currenciesMapper = [
+      {
+        code: 'USD',
+        name: 'US Dollar',
+        bid: 3.7148,
+        pctChange: -0.16,
+        create_date: '2023-10-11 11:52:17',
+      },
+    ];
+    fixture.detectChanges();
+    const cardTitleElement =
+      fixture.nativeElement.querySelector('.card__title');
+    expect(cardTitleElement.textContent).toContain('US Dollar');
   });
 });

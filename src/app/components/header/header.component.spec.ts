d691267db96c7f6d3d6291e90bf24817fd2862ac
Malu <malufell@hotmail.com>
2023-10-11 12:46:08 -0300

    test: add unit tests to header component

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +9,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -17,5 +18,18 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the logo', () => {
+    const logoElement = fixture.debugElement.query(By.css('img'));
+    expect(logoElement).toBeTruthy();
+    expect(logoElement.nativeElement.src).toContain('assets/logo.svg');
+  });
+
+  it('should have alt text for the logo', () => {
+    const logoElement = fixture.debugElement.query(By.css('img'));
+    expect(logoElement.nativeElement.alt).toBe(
+      'Moedas na cor dourada ao lado do texto Currency Converter',
+    );
   });
 });
